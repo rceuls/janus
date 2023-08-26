00e40b91c2c7d55d1fefc75fec32d34625bc1505
Raf Ceuls <rceuls@gmail.com>
2023-08-26 23:40:20 +0200

    fixed search

--- data-parser.js
+++ data-parser.js
@@ -7,11 +7,12 @@ function getBranch(tree, zone, flatStructure, iter) {
         .map(x => x['zone'])));
 
     for (const rz of rootZones) {
+        const branch = getBranch(tree, rz, flatStructure, iter + 1);
         relevantChildren.push({
             zone: rz.includes(",") ? rz.split(",").slice(-1) : rz,
             id: `zone_${rz}`,
-            "_children": getBranch(tree, rz, flatStructure, iter + 1),
-            searcher: rz.toLocaleLowerCase()
+            "_children": branch,
+            searcher: branch.map(x => x.searcher).join(",")
         })
     }
     return relevantChildren;
@@ -21,11 +22,12 @@ export function getTreeDataStructure(flatStructure) {
     const rootZones = Array.from(new Set(flatStructure.map(x => x["zone"].split(",")[0])));
     const tree = [];
     for (const zone of rootZones) {
+        const branch = getBranch(tree, zone, flatStructure, 1);
         tree.push({
             zone: zone,
             id: `zone_${zone}`,
-            "_children": getBranch(tree, zone, flatStructure, 1),
-            searcher: zone.toLocaleLowerCase()
+            "_children": branch,
+            searcher: branch.map(x => x.searcher).join(",")
         });
     }
     return tree;
