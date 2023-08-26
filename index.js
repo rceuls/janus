733a9f31a8bbd340a84dcbc5399a227a102fdffb
Raf Ceuls <rceuls@gmail.com>
2023-08-26 13:16:57 +0200

    task: people selection and only one dot per people

--- index.js
+++ index.js
@@ -30,40 +30,57 @@ function getColor() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    const pointsOfInterests = [];
-
-    const image = document.getElementById("coveredImage");
-    console.log({ image })
-
-    const canvas = new fabric.Canvas('pointsOfInterest',
-        {
-            height: image.clientHeight,
-            width: image.clientWidth,
-            allowTouchScrolling: true
-        });
-
-
-    canvas.on('mouse:up', function (options) {
-        console.log(options);
-        const color = getColor();
-        const p = canvas.getPointer();
-        const rect = new fabric.Circle({
-            top: p.y,
-            left: p.x,
-            radius: 5,
-            height: 5,
-            fill: color,
-        });
-        canvas.add(rect);
-        const checked = getCheckedPeople();
-        if (checked) {
-            if (checked in people_poi) {
-                canvas.remove(people_poi[checked])
-            }
-            people_poi[checked] = rect;
-        }
-
+const canvas = new fabric.Canvas('pointsOfInterest',
+    {
+        allowTouchScrolling: true,
+        backgroundImage: "./middle-earth.webp"
     });
+
+canvas.on('mouse:wheel', function (opt) {
+    var delta = opt.e.deltaY;
+    var zoom = canvas.getZoom();
+    zoom *= 0.999 ** delta;
+    if (zoom > 20) zoom = 20;
+    if (zoom < 0.01) zoom = 0.01;
+    canvas.setZoom(zoom);
+    opt.e.preventDefault();
+    opt.e.stopPropagation();
+})
+
+canvas.on('mouse:up', function (options) {
+    console.log(options);
+    const color = getColor();
+    const p = canvas.getPointer();
+    const rect = new fabric.Circle({
+        top: p.y,
+        left: p.x,
+        radius: 5,
+        height: 5,
+        fill: color,
+    });
+    canvas.add(rect);
+    const checked = getCheckedPeople();
+    if (checked) {
+        if (checked in people_poi) {
+            canvas.remove(people_poi[checked])
+        }
+        people_poi[checked] = rect;
+    }
+
+});
+
+window.addEventListener('resize', resizeCanvas, false);
+
+function resizeCanvas() {
+    canvas.setHeight(window.innerHeight);
+    canvas.setWidth(window.innerWidth);
+    canvas.renderAll();
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    resizeCanvas();
+
+
 
 })
