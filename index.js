6d04477218cba4c1244a0ce4449e9fd8a2c08c21
Raf Ceuls <rceuls@gmail.com>
2023-08-26 17:57:19 +0200

    added panning and labels to objects

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const canvas = new fabric.Canvas('pointsOfInterest',
     {
         allowTouchScrolling: true,
-        backgroundImage: "./middle-earth.webp"
+        // backgroundImage: "./middle-earth.webp"
+        backgroundColor: 'pink'
     });
 
 
@@ -12,26 +13,73 @@ canvas.on('mouse:wheel', function (opt) {
     zoom *= 0.999 ** delta;
     if (zoom > 20) zoom = 20;
     if (zoom < 0.01) zoom = 0.01;
-    canvas.setZoom(zoom);
+    canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
     opt.e.preventDefault();
     opt.e.stopPropagation();
+
 })
 
-canvas.on('mouse:up', function (options) {
-    const p = canvas.getPointer();
-    console.log(p);
+canvas.on('mouse:down', function (opt) {
+    var evt = opt.e;
+    if (evt.altKey === true) {
+        this.isDragging = true;
+        this.selection = false;
+        this.lastPosX = evt.clientX;
+        this.lastPosY = evt.clientY;
+    }
 });
 
+canvas.on('mouse:move', function (opt) {
+    if (this.isDragging) {
+        var e = opt.e;
+        var vpt = this.viewportTransform;
+        vpt[4] += e.clientX - this.lastPosX;
+        vpt[5] += e.clientY - this.lastPosY;
+        this.requestRenderAll();
+        this.lastPosX = e.clientX;
+        this.lastPosY = e.clientY;
+    }
+});
+
+canvas.on('mouse:up', function (opt) {
+    // on mouse up we want to recalculate new interaction
+    // for all objects, so we call setViewportTransform
+    this.setViewportTransform(this.viewportTransform);
+    this.isDragging = false;
+    this.selection = true;
+});
+
+
+function getTextColor(surroundingColor) {
+    if (['yellow'].includes(surroundingColor)) {
+        return 'black';
+    } else {
+        return 'white';
+    }
+}
+
 function createBlip(data) {
-    const blip = new fabric.Circle({
-        top: +data[9].split(",")[1],
-        left: +data[9].split(",")[0],
-        radius: 5,
-        height: 5,
+    const blip = new fabric.Rect({
+        width: 10,
+        height: 10,
+        originX: 'center',
+        originY: 'center',
         fill: data[0]
     });
-    console.log(blip);
-    canvas.add(blip);
+    var text = new fabric.Text(data[2], {
+        fontFamily: 'Calibri',
+        fontSize: 5,
+        textAlign: 'center',
+        originX: 'center',
+        originY: 'center',
+        fill: getTextColor(data[0])
+    });
+    const group = new fabric.Group([blip, text], {
+        top: +data[9].split(",")[1],
+        left: +data[9].split(",")[0],
+    })
+    console.log(group);
+    canvas.add(group);
 }
 
 function resizeCanvas() {
